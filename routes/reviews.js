e687e0b741a82d73fadde2f373645babcaee6df5
Ebe05 <ebenezerjason5@gmail.com>
2022-07-18 07:51:46 +0800

    preparing for upload

--- routes/reviews.js
+++ routes/reviews.js
@@ -4,8 +4,6 @@ const reviewValidate = require('../utilities/reviewValidate')
 const controls = require('../controllers/reviews')
 const router = express.Router({ mergeParams: true })
 
-
-
 router.post('/', isLoggedIn, reviewValidate, controls.create)
 
 router.delete('/:revId', isLoggedIn, controls.delete)
